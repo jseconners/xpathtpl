cde2a6f2b2c45305d72820fbacfb6d90255b4536
James Conners <jseconners@gmail.com>
2017-05-22 16:57:35 -0700

    Added LICENSE.txt; Removed example code; Refactored url requests out of module. This should be done by importing code. More content in README

--- README.rst
+++ README.rst
@@ -5,6 +5,33 @@ xpathtpl
 Simple xpath templating for scraping website data into
 python dictionary
 
+Examples
+-------------------------------------------
+Grab the Digg headlines
+::
+
+    import xpathtpl
+    import requests
+
+    template = {
+        'headlines': {
+            '_xpath': '//header[@class="digg-story__header"]//h2[@itemprop="headline"]',
+        }
+    }
+    content = requests.get('http://digg.com', template).content
+    headlines = xpathtpl.parse(content, template)
+    print headlines
+
+
+    > {
+    >   'headlines': [
+    >     'How Anker Is Beating Apple And Samsung At Their Own Accessory Game',
+    >     'Trump Asked Top Intelligence Officials To Push Back Against FBI Investigation',
+    >     'To Get A Sense Of How Fast Maglev Trains Travel, Watch This',
+    >     'How To Run Like A Pro In 8 Weeks'
+    >   ]
+    > }
+
 
 Author
 ------
--- xpathtpl/xpathtpl.py
+++ xpathtpl/xpathtpl.py
@@ -1,42 +1,13 @@
 ######################################################
 #
 # xpathtpl - simple xpath templating for scraping
-# web pages into python dictionary
+# web page content into python dictionary
 #
 # Written by James Conners (jseconners@gmail.com)
 #
 ######################################################
 
-import requests
 import lxml.html
-import json
-
-
-BASE_URL = 'https://www.craigslist.org/about/sites'
-
-TEST_TEMPLATES = {
-    'digg-1': {
-        'digg-stories': {
-            '_xpath': '//div[@class="digg-story__content"]',
-            'title': {
-                '_xpath': './/h2[1]',
-            },
-            'source': {
-                '_xpath': './/div[@class="digg-story__metadata-container"]/span[2]'
-            },
-            'digg-count': {
-                '_xpath': './/span[@class="js--digg-count"]'
-            },
-            'description': {
-                '_xpath': './/div[@itemprop="description"]'
-            }
-        }
-    }
-}
-
-def _page_obj(url):
-    content = requests.get(url).content
-    return lxml.html.fromstring(content)
 
 
 def _text(e):
@@ -50,12 +21,10 @@ def _text(e):
 
 def _apply_tpl(tpl, e):
     """ Apply the template tpl to HTML element e """
-    # remove processing instruction attributes
-    # xpath expression to build from
     _xpath = tpl.get('_xpath', None)
     _ukeys = tpl.get('_ukeys', False)
 
-    # get sub templates
+    # copy sub templates
     subs = dict([(s, tpl[s].copy()) for s in tpl.keys() if not s.startswith('_')])
 
     # check if this is just a named container and process sub templates
@@ -70,8 +39,7 @@ def _apply_tpl(tpl, e):
         else:
             return None
 
-    # if _ukeys is True, you always get an dictionary
-    # container with return element values as keys
+    # initialize container for xpath-matched elements
     if _ukeys:
         elms = {}
     else:
@@ -85,7 +53,7 @@ def _apply_tpl(tpl, e):
             for sn in subs.keys():
                 val[sn] = _apply_tpl(subs[sn], elm)
 
-        # no sub templates, so this could possibly
+        # no sub templates, get as single text value
         else:
             val = _text(elm)
 
@@ -95,24 +63,17 @@ def _apply_tpl(tpl, e):
         else:
             elms.append(val)
 
-    # if this is a single-item list, just
-    # return that item without the container
+    # return single text values without container
     if not _ukeys and (len(elms) == 1) and (type(elms[0]) is not dict):
         return elms[0]
     else:
         return elms
 
 
-def _template_page(page, tpl):
+def parse(bcontent, tpl):
+    """ Parse page (binary string), applying xpath template """
+    page = lxml.html.fromstring(bcontent)
     for top_name in tpl:
         tpl[top_name] = _apply_tpl(tpl[top_name], page)
     return tpl
 
-
-def parse_page(url, tpl):
-    page = _page_obj(url)
-    return _template_page(page, tpl)
-
-
-obj = parse_page('https://digg.com', TEST_TEMPLATES['digg-1'])
-print json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
